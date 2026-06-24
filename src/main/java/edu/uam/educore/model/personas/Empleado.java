9b6810f7478a34b3c9c2b9906cf97b6000d93eb5
taniaq158-rgb <taniaq158@hotmail.com>
2026-06-23 19:11:49 -0600

    Create Empleado.java
    Esta clase hereda de Persona, por lo que reutiliza los datos básicos

---
+++ src/main/java/edu/uam/educore/model/personas/Empleado.java
@@ -0,0 +1,64 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package edu.uam.educore.model.personas;
+
+/**
+ *
+ * @author tania
+ */
+
+import java.time.LocalDate;
+
+public class Empleado extends Persona {
+
+  private double salario;
+  private LocalDate fechaIngreso;
+  private TipoEmpleado tipo;
+
+  public Empleado(
+      int id,
+      String nombre,
+      String apellidos,
+      String email,
+      double salario,
+      LocalDate fechaIngreso,
+      TipoEmpleado tipo) {
+    super(id, nombre, apellidos, email);
+    this.salario = salario;
+    this.fechaIngreso = fechaIngreso;
+    this.tipo = tipo;
+  }
+
+  public double getSalario() {
+    return salario;
+  }
+
+  public void setSalario(double salario) {
+    this.salario = salario;
+  }
+
+  public LocalDate getFechaIngreso() {
+    return fechaIngreso;
+  }
+
+  public void setFechaIngreso(LocalDate fechaIngreso) {
+    this.fechaIngreso = fechaIngreso;
+  }
+
+  public void setTipo(TipoEmpleado tipo) {
+    this.tipo = tipo;
+  }
+
+  @Override
+  public String getTipo() {
+    return tipo.toString();
+  }
+
+  @Override
+  public String getInfo() {
+    return String.format(
+        "[%s] %s %s | Salario: ₡%.2f", getTipo(), getNombre(), getApellidos(), salario);
+  }
+}
