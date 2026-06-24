7026f8c32a1f63897965e994502a23388a271284
taniaq158-rgb <taniaq158@hotmail.com>
2026-06-23 20:36:32 -0600

    Create ListaEdificioRepo.java
    Creada

---
+++ src/main/java/edu/uam/educore/dao/ListaEdificioRepo.java
@@ -0,0 +1,59 @@
+
+
+
+package edu.uam.educore.dao;
+import edu.uam.educore.model.infraestructura.Edificio;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Optional;
+
+/**
+ *
+ * @author tania
+ */
+
+
+public class ListaEdificioRepo extends Repositorio<Edificio> {
+
+  private final List<Edificio> lista = new ArrayList<>();
+
+  @Override
+  public void guardar(Edificio e) {
+    lista.add(e);
+  }
+
+  @Override
+  public void actualizar(Edificio actualizado) {
+    for (int i = 0; i < lista.size(); i++) {
+      if (lista.get(i).getId() == actualizado.getId()) {
+        lista.set(i, actualizado);
+        return;
+      }
+    }
+  }
+
+  @Override
+  public void eliminar(int id) {
+    for (int i = 0; i < lista.size(); i++) {
+      if (lista.get(i).getId() == id) {
+        lista.remove(i);
+        return;
+      }
+    }
+  }
+
+  @Override
+  public Optional<Edificio> buscarPorId(int id) {
+    for (Edificio e : lista) {
+      if (e.getId() == id) {
+        return Optional.of(e);
+      }
+    }
+    return Optional.empty();
+  }
+
+  @Override
+  public List<Edificio> buscarTodos() {
+    return new ArrayList<>(lista);
+  }
+}
